9f71c10bef727ef3e83bd0fead6c22ba00d1a7b2
krunal padsala <krunalpadsala30@gmail>
2023-05-10 11:10:00 +0530

    hello world

---
+++ c program/task/mybio.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+	printf("*************************************************");
+	printf("\n*\tName: krunal padsala\t\t        *");
+	printf("\n*\tMobail No: 9512540136\t\t\t*");
+	printf("\n*\tEmail: krunalpadsala30@gmail.com\t*");
+	printf("\n*\tDOB: DEC 30,2000\t\t\t*");
+	printf("\n*\tAddress(Area): Sarthana Jakatnaka\t*");
+	printf("\n*\tcity: Surat\t\t\t\t*");
+	printf("\n*\tNationality: Indian\t\t\t*");
+	printf("\n*\tReligion: Hindu\t\t\t\t*");
+	printf("\n*\tGender: male\t\t\t\t*");
+	printf("\n*\tEducation: B.C.A (Vivekanand colleage)  *");
+	printf("\n*\tSkill:  Problem Solving\t\t\t*");
+	printf("\n*\t\tTime Managment\t\t\t*");
+	printf("\n*                                   \t\t*");
+	printf("\n*************************************************");
+}
---
+++ c program/task/myinfo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+	printf("Name: krunal padsala");
+	printf("\nMobail No: 9512540136");
+	printf("\nEmail: krunalpadsala30@gmail.com");
+	printf("\nDOB: DEC 30,2000");
+	printf("\nAddress(Area): Sarthana Jakatnaka");
+	printf("\ncity: Surat");
+	printf("\nNationality: Indian");
+	printf("\nReligion: Hindu");
+	printf("\nGender: male");
+	printf("\nEducation: B.C.A (Vivekanand colleage)");
+	printf("\nSkill:  Problem Solving\n\tTime Managment");
+}
