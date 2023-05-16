d35482d3cab7c776fcae76e801869e0ea6649f15
krunal padsala <krunalpadsala30@gmail>
2023-05-16 14:34:11 +0530

    assignment

---
+++ c program/task/flot.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main(){
+	float a,b;
+	printf("enter a flot value a :");
+	scanf("%f",&a);
+	printf("enter a float value b :");
+	scanf("%f",&b);
+	printf("enter a flot value a  %0.2f",a);  //0.2 means print a  2 digit after point. 
+	printf("\n enter a flot value b  :%0.2f",b);
+	
+	
+}
+
---
+++ c program/task/multi.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+	int a=10,b=20,c=30;
+
+	c=a*b;
+	a=b*c;
+
+	printf("c=%d\n",c);
+	printf("a=%d\n",a);
+	
+}
