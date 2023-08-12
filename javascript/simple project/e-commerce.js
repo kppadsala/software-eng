106434e4e047bc279c9633eb774a6d9b210b1091
krunal padsala <133028834+kppadsala@users.noreply.github.com>
2023-08-12 22:17:25 +0530

    Add files via upload

---
+++ javascript/simple project/e-commerce.js
@@ -0,0 +1,32 @@
+
+
+
+let myred = document.getElementById("myred");
+let img1 = document.querySelector(".sred");
+img1.addEventListener("click", function () {
+  myred.src= "image/product1.png"
+})
+
+let myblue = document.getElementById("myblue");
+let img2 = document.querySelector(".sblue");
+img2.addEventListener("click", function () {
+  myred.src = "image/product2.png"
+})
+
+let myperpol = document.getElementById("myperpol");
+let img3 = document.querySelector(".sperpol");
+img3.addEventListener("click", function () {
+  myred.src = "image/product3.png"
+})
+
+let myyellow = document.getElementById("myyellow");
+let img4 = document.querySelector(".syellow");
+img4.addEventListener("click", function () {
+  myred.src = "image/product4.png"
+})
+
+let myborwn = document.getElementById("mybrown");
+let img5 = document.querySelector(".sbrown");
+img5.addEventListener("click", function () {
+  myred.src = "image/product5.png"
+})
